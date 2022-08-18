7968b167b96ba24a72408bb951fbdf5101f6f083
Пользователь <mulitza.an@mail.ru>
2022-08-18 21:25:15 +0300

    fixes

--- src/components/AccordionScreen/Accordion/Accordion.tsx
+++ src/components/AccordionScreen/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import s from "./Accordion.module.css";
 import { ReactComponent as Cross } from "../../../images/cross.svg";
 
@@ -7,8 +7,32 @@ interface PropsType {
   content: string;
 }
 
+const hasWindow = typeof window !== "undefined";
+
 const Accordion: React.FC<PropsType> = ({ title, content }) => {
   const [isActive, setIsActive] = useState(false);
+  const [accordionContentMargin, setAccordionContentMargin] = useState(1);
+
+  const [size, setSize] = React.useState({});
+
+  const accordionContentRef = useRef<HTMLDivElement | null>(null);
+
+  const resizeHandler = () => {
+    const { clientHeight, clientWidth } = accordionContentRef.current || {};
+    setSize({ clientHeight, clientWidth });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeHandler);
+    accordionContentRef.current &&
+      setAccordionContentMargin(-accordionContentRef.current.clientHeight);
+
+    accordionContentRef.current &&
+      console.log(accordionContentRef.current.clientHeight);
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, [size]);
 
   return (
     <div className={s.accordionItem}>
@@ -19,6 +43,8 @@ const Accordion: React.FC<PropsType> = ({ title, content }) => {
         </div>
       </div>
       <div
+        ref={accordionContentRef}
+        style={!isActive ? { marginTop: accordionContentMargin } : undefined}
         className={
           isActive
             ? `${s.accordionContent}`
